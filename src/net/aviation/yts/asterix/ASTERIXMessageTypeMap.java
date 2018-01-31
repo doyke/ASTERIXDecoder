a19117727e7daf62a10a3423b1cd16fa2c16320c
Floki02 <zeroreloaded@gmail.com>
2018-01-31 16:37:07 +0900

    Add files via upload

---
+++ src/net/aviation/yts/asterix/ASTERIXMessageTypeMap.java
@@ -0,0 +1,38 @@
+/****************************************************************************
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    
+    @author T.Y.Shin (https://github.com/zeroreloaded)
+    
+******************************************************************************/
+package net.aviation.yts.asterix;
+
+import java.util.*;
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name="codes")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ASTERIXMessageTypeMap
+{
+    private Map<Integer, String> messageMap = new HashMap<Integer, String>();
+    
+    public Map<Integer, String> getMessageMap() 
+    {
+        return messageMap;
+    }
+    
+    public void setMessageMap(Map<Integer, String> messageMap)
+    {
+        this.messageMap = messageMap;
+    }
+}
